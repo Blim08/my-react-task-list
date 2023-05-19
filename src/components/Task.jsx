04948843e8fbdc3888268abffd7b1cda0406f399
Jairo Durango <cartuchoxl@hotmail.com>
2023-05-18 23:34:52 -0500

    creado hook 2

---
+++ src/components/Task.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+export const Task = (props) => {
+    const {id, descripcion, completada, onEliminar, onEditar, onCompletar} = props;
+    
+    const [modoEdicion, setModoEdicion] = useState (false);
+    const [nuevaDescripcion, setNuevaDescripcion] = useState(descripcion);
+    const [estaCompletado, setEstaCompletado] = useState(completada);
+
+    const handleEliminar = () => {
+        console.log("1")
+        onEliminar(id);
+    };
+
+    const handleEditar = () => {
+        if (modoEdicion) {
+            onEditar(id, nuevaDescripcion);
+            setModoEdicion(false);
+        }   else {
+            setModoEdicion(true);
+        }
+    };
+
+    const handlechange = (evento) => {
+        setNuevaDescripcion (evento.target.value);
+    };
+
+    const handleCompletado = () => {
+        setEstaCompletado(!estaCompletado);
+         onCompletar(id, estaCompletado);
+        
+    };
+
+    
+    return (
+        <li>
+        <input id={id} type="checkbox" checked={estaCompletado} onChange={handleCompletado}/>
+      {modoEdicion ? ( 
+        <input
+         type="text"
+         value={nuevaDescripcion}
+         onChange={handlechange}
+        />
+
+      ) : (
+        <label htmlFor={id}>{descripcion}</label>
+      )}
+      <br />
+      <button onClick={handleEditar}>{modoEdicion ? "Guardar" : "Editar"}</button>
+      <button onClick={()=>handleEliminar()}>Eliminar</button>
+        </li>
+    );
+};
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Task } from "./Task";
+
+export const TaskList = (props) => {
+    const {pendientes, onEliminar, onEditar, onCompletar} = props;
+    
+    const handleElinar = (id) => {
+       
+        onEliminar(id);
+    }
+
+    const handleEditar = (id, nuevaDescripcion)=> {
+        onEditar(id, nuevaDescripcion);
+    }
+
+
+    return (
+        <ul>
+            {pendientes.map((tarea) => (
+            <Task
+                key={tarea.id}
+                id={tarea.id}
+                descripcion={tarea.descripcion}
+                completada={tarea.completada}
+                onEliminar={handleElinar}
+                onEditar={handleEditar}
+                onCompletar={onCompletar}
+            />
+            ))}
+        </ul>
+    );
+  }
